e90a96a8e1775fced61c82f499053228a82cab7a
Brandee Cheung <brandeedcheung@gmail.com>
2024-04-25 18:45:41 -0500

    fixed toggle emoji feature

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,6 @@
 const ld_blog_button = document.getElementById('ldmode-blog');
 const ld_index_button = document.getElementById('ldmode-index');
+const ldmode = document.getElementById('ldmode-index');
 
 if (ld_blog_button) {
     ld_blog_button.addEventListener('click', toggle_light_dark);
@@ -12,4 +13,14 @@ if (ld_index_button) {
 function toggle_light_dark() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    
+    const demoElement = document.getElementById("ldmode-index");
+    const result = ""; // Replace "😊" with the emoji you want to use
+
+    if (ldmode.textContent === "🌞") {
+        demoElement.innerHTML = "🌘";
+    } else {
+        ldmode.textContent = "🌘";
+        ldmode.textContent = "🌞";
+    }
 }
